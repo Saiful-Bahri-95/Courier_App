8defae298a18e8d014c4c74261be0719d45f76ae
unknown <saiful.bahri2004@gmail.com>
2026-04-13 11:39:28 +0700

    update global variable

--- lib/config/globar_variable.dart
+++ lib/config/globar_variable.dart
@@ -1,6 +1,5 @@
 //String uri = 'http://10.0.2.2:3000';
 
 class ApiConfig {
-  static const String baseUrl =
-      'https://backendcourierapp-production.up.railway.app';
+  static const String baseUrl = 'https://backendcourierapp.up.railway.app';
 }
